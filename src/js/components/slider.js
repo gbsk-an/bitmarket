f5b18e6398f599d5777ee0b246f762e69658a373
Anna Gubskaya <gubskaya.ann@gmail.com>
2022-08-22 17:54:31 +0500

    Work on responsive design

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -10,3 +10,16 @@ const swiper = new Swiper('.crypto-news__content', {
     prevEl: '.crypto-news__slider-btn--prev',
   },
 });
+
+// import Swiper, { Navigation, Scrollbar } from 'swiper';
+
+// Swiper.use([Navigation, Scrollbar]);
+
+// const swiper = new Swiper('.crypto-news__content', {
+//   slidesPerView: 'auto',
+//   loop: true,
+//   navigation: {
+//     nextEl: '.crypto-news__slider-btn--next',
+//     prevEl: '.crypto-news__slider-btn--prev',
+//   },
+// });
